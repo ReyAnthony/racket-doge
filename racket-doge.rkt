fe5be845e79aac7f65cace6ffb1a2727284b27cd
Anthony Rey <anthony.rey5@gmail.com>
2015-11-06 00:08:06 +0100

    Added frame and random generated dogeries

--- racket-doge.rkt
+++ racket-doge.rkt
@@ -2,6 +2,8 @@
 ;Anthony REY
 ;5/11/15
 #lang racket
+(require racket/gui
+         racket/draw)
 
 ;keywords definitions
 (define (doge-keywords)
@@ -12,31 +14,63 @@
   '("Anthony" "awesome" "great"
               "nice" "cool" "programming god"
               "lisper" "lambda" "C++ hater" "doge"))
+(define (colors)
+  '("red" "pink" "blue"))
 
-;gives a random member from a list
 (define (random-word my-list)
+  ;gives a random member from a list
   (car
    (shuffle
     (my-list))))
 
-;make a sentence 
 (define (make-sentence keyword)
+  ;make a sentence 
   (string-append
    (random-word doge-keywords)
    " "
    keyword))
 
-;add max count
-;make a list of sentences
+;TODO add max count
 (define (make-sentences my-list)
-  (cons (wow)
-        (map make-sentence
-             (shuffle(my-list)))))
+  ;make a list of sentences
+  (if (null? (my-list))
+      ;return a list containing this message
+      '("Empty set of keywords !")
+      (cons (wow) ;else
+            (map make-sentence
+                 (shuffle (my-list))))))
 
-;print result
+(define doge
+  (read-bitmap "plainDoge.jpg"))
+
+;drawing on doge
+(define text-target doge)
+(define dc (new bitmap-dc% [bitmap text-target]))
+(send dc set-font (make-font #:size 25 #:family 'decorative))
+
+;actually drawing text
 (for-each
- (lambda (value)
-   (display value)
-   (display "\n"))
+ (lambda (kw)
+     (send dc set-brush (random-word colors) 'panel)
+     (send dc draw-text kw (random 700) (random 500)))
  (make-sentences user-keywords))
+
+;frame
+(define f (new frame%
+               [label "racket-doge"]
+               [width 700]
+               [height 500]
+               [style (list 'no-resize-border)]))
+;canvas
+(define my-canvas
+  (new canvas% [parent f]
+     [paint-callback
+      (lambda (canvas dc)
+        (send dc draw-bitmap doge 0 0))]))
+
+(send f show #t)
+
+
+
+
 

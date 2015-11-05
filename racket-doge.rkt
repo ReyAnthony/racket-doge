8a2a5a7287aafd3604ced83ad72f495a2136524d
Anthony Rey <anthony.rey5@gmail.com>
2015-11-05 19:26:14 +0100

    initial commit

---
+++ racket-doge.rkt
@@ -0,0 +1,42 @@
+;Doge generator
+;Anthony REY
+;5/11/15
+#lang racket
+
+;keywords definitions
+(define (doge-keywords)
+  '("such" "much" "so" "very"))
+(define (wow)
+  "wow")
+(define (user-keywords)
+  '("Anthony" "awesome" "great"
+              "nice" "cool" "programming god"
+              "lisper" "lambda" "C++ hater" "doge"))
+
+;gives a random member from a list
+(define (random-word my-list)
+  (car
+   (shuffle
+    (my-list))))
+
+;make a sentence 
+(define (make-sentence keyword)
+  (string-append
+   (random-word doge-keywords)
+   " "
+   keyword))
+
+;add max count
+;make a list of sentences
+(define (make-sentences my-list)
+  (cons (wow)
+        (map make-sentence
+             (shuffle(my-list)))))
+
+;print result
+;issue in command line, will not print \n's
+(for-each
+ (lambda (value)
+   (printf value)
+   (printf "\n"))
+ (make-sentences user-keywords))

4a896ef77e5cc32e494a56b7de5d7b4a550f2fb0
Anthony Rey <anthony.rey5@gmail.com>
2015-11-06 22:00:19 +0100

    More functionnal way

--- racket-doge.rkt
+++ racket-doge.rkt
@@ -21,7 +21,7 @@
 ;keywords definitions
 (define doge-keywords
   '("such" "much" "so" "very"))
-(define (wow)
+(define wow
   "Wow")
 (define user-keywords
   '("purr" "claw" "hit" "cate" "mean"
@@ -48,88 +48,101 @@
       ;return a list containing this message
       '("Empty set of keywords !")
       ;else
-      (cons (wow) 
+      (cons wow 
             (map make-sentence
                  (shuffle my-list)))))
-
+     
 ;frame
-(define parent-frame
+(define (create-parent-frame)
   (new frame%
        [label "racket-doge"]
        [width 700]
        [height 600]
        [style (list 'no-resize-border)]))
 
-(define doge
-  (read-bitmap "plainDoge.jpg"))
-
 ;canvas
-(define main-canvas
+(define (main-canvas parent-frame to-draw)
   (new canvas% [parent parent-frame]
        [min-height 500]
        [paint-callback
         (lambda (canvas dc)
-          (send dc draw-bitmap doge 0 0))]))
+          (send dc draw-bitmap to-draw 0 0))]))
 
 ;horiz panel
-(define horiz-panel
+(define (create-horiz-panel parent-frame)
   (new horizontal-panel% [parent parent-frame]))
 
-;setting up the text drawing on doge
-;we will draw on doge
-(define my-dc (new bitmap-dc% [bitmap doge]))
-(define (set-font)
+;font management
+(define (set-font my-dc)
   (send my-dc set-font
       (make-font #:size (+ 20 (random 5))
                  #:family 'decorative
                  #:face "Comic Sans MS"
                  #:weight 'bold)))
 
-(define (draw-text-with-keywords kwd) 
+;draw on doge
+(define (draw-text-with-keywords kwd my-dc) 
   ;draw the text on my-dc
   (for-each
    (lambda (kw)
-     (set-font)
+     (set-font my-dc)
      (send my-dc set-text-foreground (random-member colors))
      (send my-dc draw-text kw (random 700) (random 500)))
    (make-sentences kwd)))
 
-(define (draw-text)
-  (draw-text-with-keywords user-keywords))
-
-(draw-text)
-
 ;text edit
-(define user-entries
+(define (create-user-entries horiz-panel)
   (new text-field%
        ;[enabled #f]
        [label "user-keywords :"]
        [init-value (string-join user-keywords ", ")]
        [parent horiz-panel]))
 
+(define (parse-user-input user-entries)
+  (string-split (send user-entries get-value) ", "))
+
 ;buttons
-(define gen-again
+(define (gen-again-but horiz-panel user-entries my-dc p-frame)
   (new button% [label "Generate again"]
        ;generate again
        [parent horiz-panel]
        ;[enabled #f]
        [callback
         (lambda (b e)
+          ;redraw not working on OSX ???
           (draw-text-with-keywords
-           (string-split (send user-entries get-value) ", "))
-          (send parent-frame show #t))]))
+           (parse-user-input user-entries) my-dc)
+           (send p-frame show #t))]))
 
-(define save
+(define (save-but doge horiz-panel)
   (new button% [label "Save"]
        ;Save the bitmap to a file
        [parent horiz-panel]
        [callback
         (lambda (b e)
-          (if
            (send doge save-file
                  "./generated_doge.jpg"
-                 'jpeg)
-           (print "Saved\n")
-           (print "Failed to save\n")))]))
+                 'jpeg))]))
 
-(send parent-frame show #t)
+
+;let's make the GUI and run the whole program
+(define (make-gui)
+  (let
+      ([p-frame (create-parent-frame)])
+    (let
+        ([doge (read-bitmap "plainDoge.jpg")])
+      (main-canvas p-frame doge)
+      (let
+          ([horiz-panel (create-horiz-panel p-frame)])
+        ;let's create a drawing context and draw on it
+        (let
+            ([my-dc (new bitmap-dc% [bitmap doge])])
+          (let
+              ([user-entries (create-user-entries horiz-panel)]) 
+            (draw-text-with-keywords
+             (parse-user-input user-entries) my-dc)
+          (gen-again-but horiz-panel user-entries my-dc p-frame)
+           (save-but doge horiz-panel))) 
+        (send p-frame show #t)))))
+  
+(make-gui)
